098d419fc3090dc95451149a969964f4a3fd0105
Hilmi Almuhtade Billah <hilmialmuhtadeb@gmail.com>
2022-10-02 02:44:57 +0700

    implement redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../features/counter/counterSlice'
 
 const Home = () => {
+  const count = useSelector(state => state.counter.value)
+  const dispatch = useDispatch()
+  
   return (
-    <div>
-
+    <div className='container'>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <p>{ count }</p>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
     </div>
   )
 }

1fd04815fc1639e5c155aea354cbda78bbe6b9d8
Hilmi Almuhtade Billah <hilmialmuhtadeb@gmail.com>
2022-10-01 15:27:33 +0700

    initiate project

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppRoute from './routes'
+
+import './main.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppRoute />
+  </React.StrictMode>
+)
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">
+        Ini home ya kidz
+      </p>
+    </div>
+  )
+}
+
+export default Home
